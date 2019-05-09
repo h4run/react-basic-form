cf6e2d8132671dab5d3222f822fc0f5ad28abe2c
Harun Memiş <harunn.memis@gmail.com>
2019-05-09 10:20:36 +0300

    add react depency

--- src/FormElement/CheckboxList.js
+++ src/FormElement/CheckboxList.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import RadioList from './RadioList';
 
 const CheckboxList = props => <RadioList {...props} name={`${[props.name]}[]`} type="checkbox" />;
--- src/FormElement/RadioList.js
+++ src/FormElement/RadioList.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const RadioList = ({
   defaultValue, id, options, ...props
 }) => {
